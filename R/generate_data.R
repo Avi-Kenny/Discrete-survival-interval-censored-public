4becf78cb0070eb1c4a27bf037a9260e7c7a32c8
Avi Kenny <avi.kenny@gmail.com>
2025-10-05 10:31:46 -0400

    Bug fix with cal time variable

--- R/generate_data.R
+++ R/generate_data.R
@@ -25,7 +25,7 @@ generate_data <- function(n, max_time, par) {
   w_2 <- sample(c(13:60), size=n, replace=T)
   
   # Scale age variable
-  w_2 <- w_2/100
+  w_2 <- scale_age(w_2)
   
   # Sample start times
   s_i <- sample(c(1:max_time), size=n, replace=T)
@@ -43,7 +43,7 @@ generate_data <- function(n, max_time, par) {
     w_1_ <- w_1[i]
     w_2_ <- w_2[i]
     s_i_ <- s_i[i]
-    cal_time_sc <- s_i_/10
+    cal_time <- s_i_
     
     while (!event && j<=max_time) {
       
@@ -51,11 +51,11 @@ generate_data <- function(n, max_time, par) {
       if (j==1) {
         # Sample baseline serostatus
         p_sero <- icll(
-          p$a_s + p$g_s[1]*w_1_ + p$g_s[2]*w_2_ + p$t_s*cal_time_sc
+          p$a_s + p$g_s[1]*w_1_ + p$g_s[2]*w_2_ + p$t_s*cal_time
         )
       } else {
         p_sero <- x_prev + (1-x_prev) * icll(
-          p$a_x + p$g_x[1]*w_1_ + p$g_x[2]*w_2_ + p$t_x*cal_time_sc
+          p$a_x + p$g_x[1]*w_1_ + p$g_x[2]*w_2_ + p$t_x*cal_time
         )
       }
       x[j] <- x_prev <- rbinom(n=1, size=1, prob=p_sero)
@@ -72,7 +72,7 @@ generate_data <- function(n, max_time, par) {
       
       # Sample events
       p_event <- icll(
-        p$a_y + p$g_y[1]*w_1_ + p$g_y[2]*w_2_ + p$t_y*cal_time_sc +
+        p$a_y + p$g_y[1]*w_1_ + p$g_y[2]*w_2_ + p$t_y*cal_time +
           p$beta_x*x[j]
       )
       
@@ -81,7 +81,7 @@ generate_data <- function(n, max_time, par) {
       
       # Increment time-varying variables
       j <- round(j+1)
-      cal_time_sc <- cal_time_sc + 0.1
+      cal_time <- cal_time + 1
       w_2_vec <- c(w_2_vec, w_2_)
       w_2_ <- w_2_ + 0.01
       
@@ -89,20 +89,23 @@ generate_data <- function(n, max_time, par) {
     
     # Need to subtract one from j since it is incremented at the end of the loop
     j <- j-1
-
+    
+    # End time variable (t_i)
+    t_i <- cal_time - 1
+    
     # Calculate time(s) of most recent negative test and/or positive test
-    T_pm <- T_plusminus(s_i=s_i_, t_i=j+s_i_-1, x=x, v=v)
+    T_pm <- T_plusminus(s_i=s_i_, t_i=t_i, x=x, v=v)
     
     # Calculate case indicators
     case_i <- case(T_pm$T_minus, T_pm$T_plus)
 
     # Calculate Delta
-    d <- g_delta(case=case_i, s_i=s_i_, t_i=s_i_+j-1, T_minus=T_pm$T_minus,
+    d <- g_delta(case=case_i, s_i=s_i_, t_i=t_i, T_minus=T_pm$T_minus,
                  T_plus=T_pm$T_plus)
     
     # Add results to dataframe
     dat <- rbind(dat, list(
-      id=rep(i,j), t_end=c(s_i_:(s_i_+j-1)), w_1=rep(w_1_,j), w_2=w_2_vec,
+      id=rep(i,j), t_end=c(s_i_:t_i), w_1=rep(w_1_,j), w_2=w_2_vec,
       y=y, v=v, delta=d, u=x*d # x=x, 
     ))
     
@@ -111,7 +114,7 @@ generate_data <- function(n, max_time, par) {
     vec_T_plus[i] <- T_pm$T_plus
     vec_case[i] <- case_i
     vec_s_i[i] <- s_i_
-    vec_t_i[i] <- j+s_i_-1
+    vec_t_i[i] <- t_i
     
   }
   
